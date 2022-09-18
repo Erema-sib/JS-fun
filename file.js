0b05479470b412b76ccba537501dd32f3736e4db
Erema-sib <eremasib@mail.ru>
2022-09-18 21:14:46 +0700

    js part-2

--- file.js
+++ file.js
@@ -1,10 +1,23 @@
 const getNumber = (max = 11, min = 0) => Math.floor(Math.random() * (max - min) + min);
+const optimizeNumber = (n) => (n + "").length >= 2 ? n : "0" + n;
 
 const data = [];
+const colors = ["#FE8072aa", "#7267E6aa", "#A3D8CFaa", "#F0B2ABaa", "#73615Eaa", "#73E667aa", "#02A298aa", "#FFE643aa", "#F0B2ABaa"];
 let n = getNumber(41, 1);
 
 while(n--) {
-    data.push(getNumber(100))
+   data.push(getNumber(100))
 }
 
+
+
 console.log(data);
+
+const box = document.querySelector(".box");
+
+for (let val of data) {
+    box.innerHTML += `<div 
+        class="card" 
+        style="background-color: ${colors[getNumber(colors.length - 1)]}"
+    >${optimizeNumber(val)}</div>`;
+}
